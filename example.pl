2d11c14b69d9288fc468a03e7c7e452fe3cea406
pnhung177 <pnhung177@acegik.net>
2019-03-14 10:15:54 +0700

    Defines input-format, output-format options

--- example.pl
+++ example.pl
@@ -2,14 +2,36 @@
 
 use JSON;
 
-my $store = bootstrap();
+# import environment varialbes & input data
+my %args = ("input-format" => "json", "output-format" => "json");
+my $store = bootstrap(\%args);
 
+# body of program: processing something here
+# .....
+
+# suppose the $store is output in this example
 my $encoder = JSON->new->utf8->pretty;
-my $output = $encoder->encode($store);
+if ($args{"output-format"} eq "json") {
+  my $output = $encoder->encode($store);
+  print "$output";
+} else {
+  foreach $key (keys $store) {
+    printf("%s:\n", uc $key);
+    my $data = $store->{$key};
+    my $type = ref($data);
+    if ($type eq 'HASH' or $type eq 'ARRAY') {
+      print $encoder->encode($data);
+    } else {
+      print $data;
+    }
+    print "\n\n";
+  }
+}
 
-print "$output\n";
+# ------------------------------------------------------ subroutines
 
-sub bootstrap() {
+sub bootstrap {
+  my $args = @_[0];
   my $decoder = JSON->new->utf8;
   my %store = ();
 
@@ -26,7 +48,10 @@ sub bootstrap() {
   }
 
   if (length $input > 0) {
-    $store{"input"} = $decoder->decode($input);
+    $store{"input"} = $input;
+    if ($args{"input-format"} eq "json") {
+      $store{"input"} = $decoder->decode($input);
+    }
   }
 
   return \%store;
